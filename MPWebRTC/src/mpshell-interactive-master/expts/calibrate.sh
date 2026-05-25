97c202b42ba0ff94b958c9d25a2389af10047bb6
ttk <2330648144@qq.com>
2026-05-25 12:55:42 +0800

    Remove MPWebRTC directory

--- MPWebRTC/src/mpshell-interactive-master/expts/calibrate.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-tcpdump -ttttt -n -r opt.pcap dst 100.64.0.2 and src 128.30.77.91 and port 80  | cut -f 1 -d ' '  | cut -f 3 -d ':' | python mult.py > tcprerun
--- MPWebRTC/src/mpshell-interactive-master/expts/mult.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python
-import sys
-fh=sys.stdin
-for line in fh.readlines():
-  ts = float(line.strip().split()[0])
-  print int(round(ts*1000.0))
